3bff4c6eaf080ae80a64547e9b0c748fee38ea6f
Rohanre1813 <138598589+rohanre1813@users.noreply.github.com>
2025-11-01 18:15:59 +0530

    Update axios baseURL for development and production

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,13 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL + "/api",
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5001/api"
+      : "https://chatty-1-62ld.onrender.com/api",
   withCredentials: true,
 });
+
 
 
 // import axios from "axios";
