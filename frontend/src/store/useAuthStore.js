d59ce9920ac6260fdd4520950ba67f415d459f8c
Rohanre1813 <138598589+rohanre1813@users.noreply.github.com>
2025-11-01 17:32:39 +0530

    Refactor BASE_URL assignment in useAuthStore.js
    Updated BASE_URL to use VITE_BACKEND_URL environment variable.

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,10 +3,12 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL =
+const BASE_URL = import.meta.env.VITE_BACKEND_URL;
+
+/*const BASE_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:5001"
-    : "https://chatty-0xt8.onrender.com";
+    : "https://chatty-0xt8.onrender.com";*/
 
 
 export const useAuthStore = create((set, get) => ({
